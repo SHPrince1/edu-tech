b93c590d85fddcbeb30ffe1842cb09f45f2dca29
Sylvanus Prince <princehsylvanus@gmail.com>
2022-11-15 15:23:23 +0100

    course components added

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -3,74 +3,83 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { TbIcons } from "react-icons/tb";
 // import 'rsuite/dist/styles/rsuite-default.css';
-import { Sidenav, Nav, Dropdown } from "rsuite";
-import style from '../styles/sidebar.module.css';
+import { Sidenav, Nav, Dropdown, Toggle } from "rsuite";
+import style from "../styles/sidebar.module.css";
 
+//
+
+import DashboardIcon from "@rsuite/icons/legacy/Dashboard";
+import GroupIcon from "@rsuite/icons/legacy/Group";
+import MagicIcon from "@rsuite/icons/legacy/Magic";
+import GearCircleIcon from "@rsuite/icons/legacy/GearCircle";
 
 //
 
 import "rsuite/dist/rsuite.min.css";
 
-import UserBadgeIcon from "@rsuite/icons/UserBadge";
-import DashboardIcon from "@rsuite/icons/Dashboard";
 import SettingIcon from "@rsuite/icons/Setting";
 const Sidebar = () => {
+  const [expanded, setExpanded] = React.useState(true);
+  const [activeKey, setActiveKey] = React.useState("1");
   return (
-    <div
-      style={{
-        display: "block",
-        width: 250,
-        paddingLeft:0,
-        
-        height: 250,
-        
-       
-      }}
-    >
-      
-      <Sidenav  defaultOpenKeys={["3", "4"]} activeKey="1"  className={style.allNav}>
-        <Sidenav.Body >
-          <Nav className={style.side}>
+    <div className={style.sideNavBox}>
+      <Toggle
+        onChange={setExpanded}
+        checked={expanded}
+        checkedChildren="Collapse"
+        unCheckedChildren="Expand"
+      />
+
+      <Sidenav
+        expanded={expanded}
+        defaultOpenKeys={["3", "4"]}
+        className={style.allNav}
+      >
+        <Sidenav.Body>
+          <Nav
+            activeKey={activeKey}
+            onSelect={setActiveKey}
+            className={style.side}
+          >
             <Nav.Item eventKey="1" icon={<DashboardIcon />}>
-            <Link to ="/">Dashboard</Link>
+              <Link to="/">Dashboard</Link>
             </Nav.Item>
-
-            <Dropdown eventKey="4" title="Courses">
-              <Dropdown.Item eventKey="4-1">Register</Dropdown.Item>
-              <Dropdown.Item eventKey="4-2">
-                <a href="">View</a>
-              </Dropdown.Item>
-            </Dropdown>
-
-            <Nav.Item eventKey="2" icon={<UserBadgeIcon />}>
-              <Link to ="/profile">Profile</Link>
+            <Nav.Item eventKey="2" icon={<GroupIcon />}>
+              User Group
             </Nav.Item>
+            <Nav.Menu
+              placement="rightStart"
+              eventKey="3"
+              title="Courses"
+              icon={<MagicIcon />}
+            >
+              <Nav.Item eventKey="3-1">Register Course</Nav.Item>
+              <Nav.Item eventKey="3-2">View Courses</Nav.Item>
+            </Nav.Menu>
+            <Nav.Menu
+              placement="rightStart"
+              eventKey="4"
+              title="Test"
+              icon={<GearCircleIcon />}
+            >
+              <Nav.Item eventKey="4-1">Take Test</Nav.Item>
+            </Nav.Menu>
 
-            <Nav.Item eventKey="3" icon={<SettingIcon />}>
-              Settings
-            </Nav.Item>
-
-            <Dropdown eventKey="5" title="Advanced">
-              <Dropdown.Item eventKey="5-1">Privacy</Dropdown.Item>
-              <Dropdown.Item eventKey="5-2">About</Dropdown.Item>
-              <Dropdown.Item eventKey="5-3">Terms</Dropdown.Item>
-            </Dropdown>
-            <Nav.Item eventKey="3" icon={<SettingIcon />}>
-              Settings
-            </Nav.Item>
-
-            <Dropdown eventKey="6" title="Advanced">
-              <Dropdown.Item eventKey="6-1">Privacy</Dropdown.Item>
-              <Dropdown.Item eventKey="6-2">About</Dropdown.Item>
-              <Dropdown.Item eventKey="6-3">Terms</Dropdown.Item>
-            </Dropdown>
+            <Nav.Menu eventKey="4-5" title="Lecturer">
+              <Nav.Item eventKey="4-5-1">Register</Nav.Item>
+              <Nav.Item eventKey="4-5-2">Add Test</Nav.Item>
+              <Nav.Menu eventKey="5-6" title="Materials">
+                <Nav.Item eventKey="6-5-1">BOOKS</Nav.Item>
+                <Nav.Item eventKey="6-5-2"> ASSIGNMENTS</Nav.Item>
+              </Nav.Menu>
+            </Nav.Menu>
           </Nav>
         </Sidenav.Body>
+        {/* <Sidenav.Toggle
+          expanded={expanded}
+          onToggle={(expanded) => setExpanded(expanded)}
+        /> */}
       </Sidenav>
-
-      
-
-      
     </div>
   );
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CourseComponents from '../components/CourseComponents'
 
 function Home() {
   return (
@@ -6,6 +7,7 @@ function Home() {
       <h1>
         HOME
       </h1>
+      <CourseComponents/>
     </div>
   )
 }
