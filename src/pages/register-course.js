3029f5ef51564ed0da23d99cac8e8925313f4e99
Sylvanus Prince <princehsylvanus@gmail.com>
2022-11-21 13:17:27 +0100

    new pages added

---
+++ src/pages/register-course.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CourseReg from "../components/coursereg"
+
+const RegisterCoursePage = () => {
+  return (
+    <div>
+        < CourseReg />
+    </div>
+  )
+}
+
+export default RegisterCoursePage
--- src/pages/students-login.js
+++ src/pages/students-login.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Loginc from "../components/loginc";
 import Footer from "../components/footer";
+import style from "../styles/lecturer.module.css"
+
 
 const Login = () => {
   return (
     <div>
-      <Loginc />
-      <div>
-        {/* <Footer /> */}
+       <div className={style.header}>
+        <h6>ENTER STUDENT DETAILS</h6>
       </div>
+
+      <Loginc />
+      <Footer />
+      
     </div>
   );
 };
